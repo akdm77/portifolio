929aa8f542d509f0b07648ff3a8977ce6e4f3038
akdm77 <alankdm77@gmail.com>
2025-02-15 19:14:19 -0300

    portifolio

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,70 +1,92 @@
-"use client"
-import Image from 'next/image'
-import Link from 'next/link'
-import { useState } from 'react'
-import menuClose from '/public/icon-close-menu.svg'
-import menu from '/public/icon-menu.svg'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import menuClose from "/public/icon-close-menu.svg";
+import menu from "/public/icon-menu.svg";
 
 const Navbar = () => {
-  const[ativo, setAtivo] = useState(false)  
+  const [ativo, setAtivo] = useState(false);
+  const OpenPagesAndClosePages = () => {
+    setAtivo(ativo == true)
+
+    setAtivo(ativo == false)
+  }
   return (
-    <nav className='flex w-full fixed top-0 left-0 right-0 z-0 h-auto bg-violet-400  text-white justify-end md:h-20 lg:h-20'>
-      <Link href="/"><h2 className='text-2xl text-cyan-600 font-bold p-6 md:pl-11 fixed left-2'>Logo</h2></Link>
-      <div className=' px-4 mx-auto lg:max-w-7xl  md:flex md:px-8 '>
+    <nav className="flex  fixed top-0 left-0 right-0 z-0 h-auto  bg-[#2C3E50] items-center  text-white justify-end md:h-20 lg:h-20">
+      <Link href="/">
+        <Image
+          src="/AKDM logo with the name larger and in white color.png"
+          height={100}
+          width={100}
+          alt="AKDM"
+          className="fixed left-5 top-7 size-16 border rounded-full lg:top-2 sm:top-1"
+          unoptimized
+        />
+      </Link>
+      <div className=" px-4 mx-auto lg:max-w-7xl  md:flex md:px-8 "></div>
+
+      <div
+        className={`flex justify-self-center   pb-3 md:block md:pb-0 md:mt-0 ${ativo
+            ? " relative  left-4 top-24 min-w-full  md:p-8 text-center  bg-gradient-to-b from-[#2C3E50] via-[#ECF0F1] via-[#E67E22] to-[#1ABC9C] "
+            : "hidden"
+          }`}
+      >
+        <ul
+          className=" h-screen  md:fixed md:right-6 md:top-0 md:h-auto md:flex  md:justify-end  md:pt-3.5 md:items-start 
+          flex-grow items-center justify-center w-2/3  text-center "
+          onClick={OpenPagesAndClosePages}
+        >
+          <Link href="#about" className="w-28" onClick={OpenPagesAndClosePages}>
+            <li className="pb-6 text-xl  text-white py-2 md:px-6 text-center items-center w-full justify-center 
+            border-b-2 md:border-b-0  hover:bg-purple-600">
+              about
+            </li>
+          </Link>
+          <Link href="#blog" onClick={OpenPagesAndClosePages}>
+            <li className="pb-6 text-xl text-white py-2 md:px-6 text-center items-center justify-center border-b-2 
+            md:border-b-0 hover:bg-purple-600">
+              blog
+            </li>
+          </Link>
+          <Link href="#projects" onClick={OpenPagesAndClosePages}>
+            <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 items-center justify-center 
+            md:border-b-0 hover:bg-purple-600">
+              projects
+            </li>
+          </Link>
+          <Link href="#contact" onClick={OpenPagesAndClosePages}>
+            <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 items-center justify-center 
+            md:border-b-0 hover:bg-purple-600">
+              contact
+            </li>
+          </Link>
+        </ul>
       </div>
-      
-      <div className={`flex justify-self-center   pb-3 md:block md:pb-0 md:mt-0 ${
-      ativo ? ' relative  left-4 top-24 min-w-full  md:p-8 text-center   ' : 'hidden'
-    }`}>
-      <ul className=' h-screen  md:fixed md:right-6 md:top-0 md:h-auto md:flex md:w-full md:justify-end  md:pt-3.5 md:items-center flex-grow items-center justify-center w-2/3  text-center ' >
-        <li className='pb-6 text-xl  text-white py-2 md:px-6 text-center items-center justify-center border-b-2 md:border-b-0  hover:bg-purple-600' onClick={() => setAtivo(!ativo)}>
-          <Link href="#about" >about</Link>
-        </li>
-        <li className='pb-6 text-xl text-white py-2 md:px-6 text-center items-center justify-center border-b-2 md:border-b-0 hover:bg-purple-600'>
-          <Link href="#blog" onClick={() => setAtivo(!ativo)}>blog</Link>
-        </li>
-        <li className='pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 items-center justify-center md:border-b-0 hover:bg-purple-600'>
-          <Link href="#contact" onClick={() => setAtivo(!ativo)}>contact</Link>
-        </li>
-        <li className='pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 items-center justify-center md:border-b-0 hover:bg-purple-600'>
-          <Link href="#projects" onClick={() => setAtivo(!ativo)}>projects</Link>
-        </li>
-      </ul>
-  
-    </div>
-      <div className="md:hidden  " >
-        <button
-         className='p-2 flex'
-         onClick={() => setAtivo(!ativo)}>
-        {
-          ativo ? 
-          (<Image src={menuClose}
-            className='focus:border-none fixed right-3 active:border-none my-6 mr-3'
-            height={30}
-            width={30}
-            alt='menu-close'
-            
+      <div className="md:hidden  ">
+        <button className="p-2 flex" onClick={() => setAtivo(!ativo)}>
+          {ativo ? (
+            <Image
+              src={menuClose}
+              className="focus:border-none fixed right-3 top-2 active:border-none  my-6 mx-3 border border-white p-2 
+              size-14 rounded-full bg-slate-300"
+              height={30}
+              width={30}
+              alt="menu-close"
             />
-            
-            
-) : (
-
-          <Image src={menu}
-            className='top-0  my-6 mx-3'
-            height={30} 
-            width={30} 
-            alt='menu'/>  
-            )
-        }
+          ) : (
+            <Image
+              src={menu}
+              className="  my-6 mx-3 border  border-white p-2 size-14 rounded-full bg-slate-300"
+              height={50}
+              width={50}
+              alt="menu"
+            />
+          )}
         </button>
-
-
-        </div>
-
-    
+      </div>
     </nav>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,81 @@
-import NavBar from './components/Navbar';
+import Link from "next/link";
+import NavBar from "./components/Navbar";
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className="">
-      <div  >
-      <NavBar/>
-       <section id='about' className="flex text-5xl bg-black h-screen  text-violet-800 justify-center items-center">
-        <h1>
-          about
-        </h1>
-       </section>
-       <section id='blog' className="flex text-5xl bg-indigo-800 h-screen text-black justify-center items-center">
-        <h1>
-          blog
-        </h1>
+    <main className=" bg-gradient-to-b  from-[#2C3E50] via-[#ECF0F1] via-[#E67E22] 
+    to-[#1ABC9C] border border-8 border-[#2C3E50] ">
+      <div className="">
+        <NavBar />
+        <section
+          id="about"
+          className="flex flex-col h-screen  text-white justify-center items-center "
+        >
+          <h1 className="text-2xl m-5">Alan Klysman</h1>
+          <p className="text-xl m-5">Olá, me chamo Alan e sou um desenvolvedor</p>
         </section>
-        <section id='contact' className="flex text-5xl bg-black h-screen text-violet-800 justify-center items-center">
-          <h1>
-            contacts
-          </h1>
+        <section
+          id="blog"
+          className=" flex flex-col gap-5 h-screen text-[#1981DD] justify-center items-center"
+        >
+          <h1 className="text-5xl mb-5">stack</h1>
+          <button className="text-xl antialiased font-bold bg-teal-500  text-blue-950 w-1/2 rounded-lg border py-3 px-10 border-green-600">
+          <Image 
+          className="py-2"
+          src="/html-5.png" 
+          width={500} 
+          height={500} 
+          alt="html" />
+          HTML 5
+          </button>
+          <button className="text-xl rounded-sm border font-bold bg-teal-500 w-1/2 text-blue-950 py-3 px-10 border-green-600">
+          <Image 
+          className="py-2"
+          src="/css-3.png" 
+          width={500} 
+          height={500} 
+          alt="css" />
+          CSS 3
+          </button>
+          <button className="text-xl rounded-sm border font-bold bg-teal-500 w-1/2 text-blue-950 py-3 px-10 border-green-600">Javascript</button>
+          <button className="text-xl rounded-sm border font-bold bg-teal-500 w-1/2 text-blue-950 py-3 px-10 border-green-600">React Js</button>
+          <button className="text-xl rounded-sm border font-bold bg-teal-500 w-1/2 text-blue-950 py-3 px-10 border-green-600">Next Js</button>
+          <button className="text-xl rounded-sm border font-bold bg-teal-500 w-1/2 text-blue-950 py-3 px-10 border-green-600">Tailwind CSS</button>
         </section>
-        <section id='projects' className="flex text-5xl bg-violet-800 h-screen text-black justify-center items-center">
-          <h1>
-            projects
-          </h1>
+        <section
+          id="projects"
+          className="flex flex-col text-5xl sm:h-full h-screen text-[#1981DD] items-center mb-10 "
+        >
+          <h1 className="m-10">projects</h1>
+          <section className="grid md:grid-cols-4 overflow-scroll gap-5 m-5 ">
+            <Link href="https://google-mu-steel.vercel.app/">
+              <Image src="/google.png" width={500} height={500} alt="google" />
+            </Link>
+            <Link href="https://snap-sable.vercel.app/">
+              <Image src="/snap.png" width={500} height={500} alt="snap" />
+            </Link>
+            <Link href="https://pagina-de-login-taupe.vercel.app/">
+              <Image src="/login.png" width={500} height={500} alt="login" />
+            </Link>
+            <Link href="https://calculator-nine-beige.vercel.app/">
+              <Image
+                src="/calculadora.png"
+                width={500}
+                height={500}
+                alt="calculadora"
+              />
+            </Link>
+          </section>
+        </section>
+        <section
+          id="contact"
+          className="flex flex-col gap-3 items-center text-2xl  h-screen text-violet-800 justify-center "
+        >
+          <h1>Contatos</h1>
+          <p><span>Email:</span>alankdm77@gmail.com</p>
+          <p><span>Telefone:</span>(86)99592-8239 </p>
+          
+          
         </section>
       </div>
     </main>
